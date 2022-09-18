b732e7579a6d9161577789c49ddf62a2f0512c49
BatraAayush <67547222+BatraAayush@users.noreply.github.com>
2022-09-18 20:44:35 +0530

    fix edge cases

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const checkBtn = document.querySelector("#check");
 const output = document.querySelector("#output");
 
 checkBtn.addEventListener("click",()=>{
-    if(initialPrice.value && quantity.value && currentPrice.value) {
+    if(initialPrice.value > 0 && quantity.value > 0 && currentPrice.value > 0) {
         checkForProfitLoss(Number(initialPrice.value),Number(quantity.value),Number(currentPrice.value));
     } else {
-        output.innerHTML = "Please insert input!"
+        output.innerHTML = "Please insert valid input!"
         output.style.color = "red";
     }
 
@@ -21,7 +21,7 @@ function checkForProfitLoss(init,qty,curr) {
         let profitPercentage = (profit/(init*qty))*100;
         profit = profit.toFixed(2);
         profitPercentage = profitPercentage.toFixed(2);
-        output.innerHTML = `Congratulations! You are in profit of ${profit} and your profit pecentage is ${profitPercentage}% &#x1F911;`;
+        output.innerHTML = `Congratulations! You are in profit of &#8377; ${profit} and your profit pecentage is ${profitPercentage}% &#x1F911;`;
         output.style.color = "green";
     } else if (init === curr) {
         output.innerHTML = `No Profit No Loss &#128528;	`;
@@ -31,7 +31,7 @@ function checkForProfitLoss(init,qty,curr) {
         let lossPercentage = (loss/(init*qty))*100;
         loss = loss.toFixed(2);
         lossPercentage = lossPercentage.toFixed(2);
-        output.innerHTML = `Sorry! You are in loss of ${loss} and your loss pecentage is ${lossPercentage}% &#128542;`
+        output.innerHTML = `Sorry! You are in loss of &#8377; ${loss} and your loss pecentage is ${lossPercentage}% &#128542;`
         output.style.color = "red";
 
     }
